97e6b425fe9d53c42417d290083e88ec0c8105f0
welldo <22337938+welldo@users.noreply.github.com>
2022-01-10 12:24:39 +0800

    Update jd_redEnvelope.js

--- jd_redEnvelope.js
+++ jd_redEnvelope.js
@@ -22,7 +22,7 @@ cron "0 0,12,20,21 * * *" script-path=https://raw.githubusercontent.com/KingRan/
 
 const $ = new Env('京东领红包');
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
-let flCode = ["Slsj8oo", "StsXemf", "SCscAnP"]
+let flCode = ["SkbfH1", "StsXemf", "SCscAnP"]
 flCode = flCode[Math.floor((Math.random() * flCode.length))]
 if ($.isNode() && process.env.FLCODE3) {
     flCode = process.env.FLCODE3
